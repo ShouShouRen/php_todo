d5ad2631fa07a4b0a23b0b14193807521ce0a69d
Peter Chen <peterchen2017125@gmail.com>
2023-01-21 13:49:29 +0800

    20230121

--- todo.sql
+++ todo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2023 年 01 月 12 日 12:48
+-- 產生時間： 2023 年 01 月 21 日 06:48
 -- 伺服器版本： 10.4.27-MariaDB
 -- PHP 版本： 8.0.25
 
@@ -39,7 +39,10 @@ CREATE TABLE `status` (
 INSERT INTO `status` (`code`, `code_name`) VALUES
 (0, '進行中'),
 (1, '已完成'),
-(2, '未完成');
+(2, '未完成'),
+(3, '普通件'),
+(4, '速件'),
+(5, '最速件');
 
 -- --------------------------------------------------------
 
@@ -54,8 +57,17 @@ CREATE TABLE `todo_list` (
   `end` varchar(10) NOT NULL,
   `content` text NOT NULL,
   `created_at` datetime NOT NULL,
-  `status` int(50) NOT NULL
+  `status` int(50) NOT NULL,
+  `usetodo` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `todo_list`
+--
+
+INSERT INTO `todo_list` (`id`, `title`, `start`, `end`, `content`, `created_at`, `status`, `usetodo`) VALUES
+(1, '工作一', '10', '12', '吃飯睡覺', '2023-01-12 19:50:23', 0, 0),
+(2, '工作二', '02', '04', '睡覺', '2023-01-14 10:55:58', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -81,6 +93,26 @@ INSERT INTO `users` (`id`, `user`, `user_name`, `pw`, `role`) VALUES
 (3, 'peter', '陳暐仁', '1022', 0),
 (4, 'user01', 'user01', '1234', 1);
 
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `usetodo`
+--
+
+CREATE TABLE `usetodo` (
+  `codes` int(5) NOT NULL,
+  `codes_name` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `usetodo`
+--
+
+INSERT INTO `usetodo` (`codes`, `codes_name`) VALUES
+(0, '普通件'),
+(1, '速件'),
+(2, '最速件');
+
 --
 -- 已傾印資料表的索引
 --
@@ -105,7 +137,7 @@ ALTER TABLE `users`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `todo_list`
 --
 ALTER TABLE `todo_list`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
