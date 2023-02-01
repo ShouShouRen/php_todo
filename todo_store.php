96d0259f39e2295b4f1c9f55fecb22de3368af01
Peter Chen <peterchen2017125@gmail.com>
2023-02-01 10:07:21 +0800

    add usetodo

--- todo_store.php
+++ todo_store.php
@@ -1,7 +1,7 @@
 <?php 
     require_once("pdo.php");
     extract($_POST);
-    $sql = "INSERT INTO todo_list (title,start,end,content,created_at,status) VALUES('$title','$start','$end','$content','$now','$status')";
+    $sql = "INSERT INTO todo_list (title,start,end,content,created_at,status,usetodo) VALUES('$title','$start','$end','$content','$now','$status','$usetodo')";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     header("Location: todo_list.php");
--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 require_once("pdo.php");
 extract($_POST);
 if (isset($_POST['title'])) {
-    $sql = "UPDATE todo_list SET title='$title',start='$start',end='$end',content='$content',status='$status' WHERE id = {$id}";
+    $sql = "UPDATE todo_list SET title='$title',start='$start',end='$end',content='$content',status='$status',usetodo='$usetodo' WHERE id = {$id}";
     header("Location:todo_list.php");
 }else{
     $sql = "UPDATE todo_list SET start='$start',end='$end' WHERE id = {$id}";
