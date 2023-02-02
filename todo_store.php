88bcf6e7086bbcc3dd81e31dee2b72098ec1dc69
Peter Chen <peterchen2017125@gmail.com>
2023-02-02 13:44:26 +0800

    20220202

--- todo_store.php
+++ todo_store.php
@@ -1,7 +1,3 @@
 <?php 
-    require_once("pdo.php");
-    extract($_POST);
-    $sql = "INSERT INTO todo_list (title,start,end,content,created_at,status,usetodo) VALUES('$title','$start','$end','$content','$now','$status','$usetodo')";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    header("Location: todo_list.php");
+require("function.php");
+StoreList();
